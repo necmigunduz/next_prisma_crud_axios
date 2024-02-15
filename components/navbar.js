e5ee8c62e5eed421b040b939b2081aa135432455
Necmi Gunduz <necmigunduz@gmail.com>
2024-02-15 17:52:01 +0300

    add: style to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -2,6 +2,8 @@ import { useRouter } from "next/router";
 import React from "react";
 import Link from "next/link";
 import { FaTasks } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+import { LiaTasksSolid } from "react-icons/lia";
 
 const Navbar = () => {
   const router = useRouter();
@@ -9,27 +11,37 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="flex items-center justify-between">
-      <Link href={"/"}><div className="flex flex-row p-4">
-          <FaTasks color="white" className="text-3xl"/>
-          <div className="text-white text-lg font-semibold px-6">TASK LIST</div>
-        </div></Link>
-        <div className="flex space-x-4">
-          <Link
-            href="/"
-            className={`text-white hover:text-gray-300 cursor-pointer ${
-              router.pathname === "/" && "border-b-2 border-white"
-            }`}
-          >
-            Home
-          </Link>
-          <Link
-            href="/tasks"
-            className={`text-white hover:text-gray-300 cursor-pointer ${
-              router.pathname.includes("/tasks") && "border-b-2 border-white"
-            }`}
-          >
-            Tasks
-          </Link>
+        <Link href={"/"}>
+          <div className="flex flex-row p-4">
+            <FaTasks color="white" className="text-3xl" />
+            <div className="text-white text-lg font-semibold px-6 hover:text-blue-300">
+              TASK LIST
+            </div>
+          </div>
+        </Link>
+        <div className="flex space-x-5">
+          <div className="flex space-x-2 items-center">
+            <FaHome color="white" className="text-xl" />
+            <Link
+              href="/"
+              className={`text-white hover:text-gray-300 cursor-pointer ${
+                router.pathname === "/" && "border-b-2 border-white hover:border-blue-300"
+              } hover:font-bold hover:text-blue-600`}
+            >
+              Home
+            </Link>
+          </div>
+          <div className="flex flex-row space-x-2 items-center">
+            <LiaTasksSolid color="white" className="text-xl" />
+            <Link
+              href="/tasks"
+              className={`text-white hover:text-gray-300 cursor-pointer ${
+                router.pathname.includes("/tasks") && "border-b-2 border-white hover:border-blue-600"
+              } hover:font-bold hover:text-blue-300`}
+            >
+              Tasks
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
